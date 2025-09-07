5a47eba87ca398b2895c3a51c6083b1ecbeb0948
Nathan Yockey <93102472+theYockaman@users.noreply.github.com>
2025-09-07 17:02:01 -0500

    update config files

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-with open("requirements.txt", "r") as f:
-    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
-setup(
-    name = "package_name"
-    , version = "0.1.0"
-    , author = "Nathan Yockey"
-    , author_email = 'n8teyock@gmail'
-    , url = ''
-    , description = "Package Description"
-    , packages = find_packages()
-    , include_package_data=True
-    , install_requires=requirements
-    , python_requires='>=3.9'
-    , test_suite='tests'
-    # , package_data = {
-    #     'utils': ['templates/HTML/*'
-    #               ,'templates/CSS/*'
-    #               ,'templates/CSS/Basics/*'
-    #               ,'templates/JS/*'
-    #               ,'templates/PHP/*'
-    #               ,'templates/MD/*'
-    #               ,'templates/Templates/*'
-    #     ]
-    # }
-    , long_description = open("README.md").read()
-    , long_description_content_type = 'text/markdown'
-    
-)
+setup()
